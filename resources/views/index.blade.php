bdc08dcde5dccc01a4857fcb08a9aae73c2f28e8
Jwan <jwanaalfatla1999@gmail.com>
2021-01-28 14:21:57 +0300

    Fourth commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -174,7 +174,7 @@
                     </div>
                 </form>
                 <div class="mt-10">
-                    <a href="/">
+                    <a href="https://www.instagram.com/iotmaker_official/">
                         <div class="mt-3 flex">
                             <svg class="fill-current text-white w-7 h-7" xmlns="http://www.w3.org/2000/svg"
                                  data-name="Layer 1" viewBox="0 0 24 24">
@@ -186,7 +186,7 @@
                             </h1>
                         </div>
                     </a>
-                    <a href="/">
+                    <a href="https://www.facebook.com/iotmakeriq">
                         <div class="mt-3 mr-2 flex">
                             <svg class="fill-current text-white w-7 h-7" xmlns="http://www.w3.org/2000/svg"
                                  data-name="Layer 1" viewBox="0 0 24 24">
@@ -194,7 +194,7 @@
                                         d="M15.12,5.32H17V2.14A26.11,26.11,0,0,0,14.26,2C11.54,2,9.68,3.66,9.68,6.7V9.32H6.61v3.56H9.68V22h3.68V12.88h3.06l.46-3.56H13.36V7.05C13.36,6,13.64,5.32,15.12,5.32Z"/>
                             </svg>
                             <h1 class="ml-4 text-blase text-white">
-                                iotmaker_offical
+                                iotmakeriq
                             </h1>
                         </div>
                     </a>
